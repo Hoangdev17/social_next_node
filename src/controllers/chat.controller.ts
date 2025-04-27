d9584976b0cd0861206ba98cede4507da368ec38
Hoangdev17 <hoangluudev17@gmail.com>
2025-04-27 21:34:00 +0700

    fix logic

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,21 +1,26 @@
 import { Request, Response } from 'express';
 import { Message } from '~/models/message.model';
 
-
 export const getMessagesBetweenUsers = async (req: Request, res: Response) => {
-  const { userId } = req.params; // 👈 lấy userId từ URL params
+  const { userId } = req.params; 
   const myId = req.userId;
+  const { skip , limit  } = req.query; 
 
   try {
+    
     const messages = await Message.find({
       $or: [
         { fromUserId: myId, toUserId: userId },
         { fromUserId: userId, toUserId: myId },
       ],
-    }).sort({ timestamp: 1 }); // sắp xếp theo thời gian tăng dần
+    })
+      .skip(Number(skip)) 
+      .limit(Number(limit)) 
+      .sort({ timestamp: 1 }); 
 
     res.json(messages);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: 'Failed to fetch messages', error });
   }
 };
